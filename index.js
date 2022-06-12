3a6cc37402a9242e565e2ef26fdef65e535ca6d5
SS <smirnovsergeyterem@gmail.com>
2022-06-12 22:59:29 +0300

    Migrate ExpressJs 3 to ExpressJs 4

--- index.js
+++ index.js
@@ -7,7 +7,24 @@ const httpPort = 3000
 const apkUpdater = require('./lib/updater.js');
 
 app.use(httpLogger)
+app.use(express.json());
 apkUpdater.enable(app, '/checkupdate');
 
 app.listen(httpPort, () =>
     logger.info('Express.js listening on port 3000.'))
+app.get('/ping', (req, res) => {
+    logger.info(req.hostname + " пинг от PeriodicWorker")
+    res.contentType("application/json")
+    res.json([
+        {
+            "Код": "000000004",
+            "Наименование": "Петров Василий",
+            "Пароль": "111"
+        },
+        {
+            "Код": "000000005",
+            "Наименование": "Пупкин Дормидонт",
+            "Пароль": "222"
+        }
+    ])
+});
