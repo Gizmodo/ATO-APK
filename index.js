64d29e05947e652b2a192cb0b8b110cf5bb0b973
SS <smirnovsergeyterem@gmail.com>
2022-06-17 02:00:33 +0300

    Fix remove update link by Timeout

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const logger = require('./log/logger')
 const httpLogger = require('./log/httpLogger')
-
+const ip = require('ip')
 const app = express()
 const httpPort = 3000
 const apkUpdater = require('./lib/updater.js');
@@ -11,9 +11,9 @@ app.use(express.json());
 apkUpdater.enable(app, '/checkupdate');
 
 app.listen(httpPort, () =>
-    logger.info('Express.js listening on port 3000.'))
+    logger.info('Сервер запущен на ' + ip.address() + ':' + httpPort))
 
 app.get('/ping', (req, res) => {
-    logger.info(req.hostname + " пинг от PeriodicWorker")
+    logger.info(req.hostname + " запрос от планировщика")
     res.json("pong")
 })
